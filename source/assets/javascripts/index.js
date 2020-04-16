c0a0a509560b9faef2aec3a0f4e6753985464a44
fouadpy <perceval@protonmail.com>
2020-04-16 23:18:56 +0200

    container video and modal effect

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -4,4 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../stylesheets/site.scss';
 import '@fortawesome/fontawesome-free/js/all';
 
+
 // Relative dependencies (our code)
+$('#video-play').click(() => {
+  const src = 'https://youtube.com/embed/MBxs2iA-EmI';
+  $('#myModal').modal('show');
+  $('#myModal iframe').attr('src', src);
+});
+
+$('#myModal').on('hidden.bs.modal', () => {
+  $('#myModal iframe').removeAttr('src');
+});
+
+// <iframe width="560" height="315" src="https://www.youtube.com/embed/kuwyUa_mxFI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
