b9c126059f871d4fbf8a90f7f9a5813a31672b7f
fouadpy <perceval@protonmail.com>
2020-04-22 14:52:19 +0200

    add block testimonial page index

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,9 +1,8 @@
 // External Library
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../stylesheets/site.scss';
 import '@fortawesome/fontawesome-free/js/all';
-
+import Glide from '@glidejs/glide';
 
 // Relative dependencies (our code)
 $('#video-play').click(() => {
@@ -18,3 +17,17 @@ $('#myModal').on('hidden.bs.modal', () => {
 
 // <iframe width="560" height="315" src="https://www.youtube.com/embed/kuwyUa_mxFI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+new Glide('.glide', {
+  perView: 3,
+  keyboard: false,
+  bound: true,
+  animationDuration: 1000,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    500: {
+      perView: 1
+    }
+  }
+}).mount();
