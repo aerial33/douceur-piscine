03a76e6429c3fc00629a2286e18a049f540366fc
fouadpy <perceval@protonmail.com>
2020-05-08 09:41:47 +0200

    js gallery picture effect

--- source/assets/javascripts/filter-gallery.js
+++ source/assets/javascripts/filter-gallery.js
@@ -1,4 +1,4 @@
-$(".link").click((event) => {
+$(".btn-filt").click((event) => {
   const tag = event.currentTarget.dataset.filter;
   if (tag === "all") {
     $(".filter").show("2000");
@@ -9,36 +9,6 @@ $(".link").click((event) => {
   }
 });
 
-
-// $(".filter-button").click(() => {
-//   const value = $(this).attr('data-filter');
-//   if (value === "all") {
-//     // $('.filter').removeClass('hidden');
-//     $('.filter').show('1000');
-//   } else {
-//     //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//     //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-//     $(".filter").not('.' + value).hide('3000');
-//     $('.filter').filter('.' + value).show('3000');
-//   }
-// });
-
-//   if ($(".filter-button").removeClass("active")) {
-// $(this).removeClass("active");
-// }
-// $(this).addClass("active");
-
-// const buttons = document.querySelector('#buttons').children;
-// const items = document.querySelector('.cards').children;
-
-// for (const button of buttons) {
-//   button.addEventListener("click", () => {
-//     // const value = button.getAttribute("data-target");
-//     for (const button of buttons){
-//       button.classList.remove("active");
-//     }
-//     button.classList.add('active');
-//     // const target = this.getAttribute("data-target");
-
-//   });
-// }
+$('.btn-filt').click(function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
